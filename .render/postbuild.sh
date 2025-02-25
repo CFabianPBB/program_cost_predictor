dedd84fae4d24c705b777bf043c2935d6383555d
CFabianPBB <chris.fabian@tylertech.com>
2025-02-24 21:42:18 -0700

    update Render folder

---
+++ .render/postbuild.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+export PATH=/opt/render/project/.venv/bin:$PATH
--- app.py
+++ app.py
@@ -269,6 +269,6 @@ def results():
                            non_personnel_allocations=non_personnel_allocations,
                            summary=summary)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
